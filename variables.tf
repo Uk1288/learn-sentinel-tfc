7ab4dea34ba3b12b3e79bf35455a929307334af3
UKEME BASSEY <ukeme.bassey@hashicorp.com>
2023-01-28 19:23:48 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,6 +13,11 @@ variable "secret_key" {
   type = string
 }
 
+variable "token" {
+  description = "AWS token"
+  type = string
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default = "t2.micro"
